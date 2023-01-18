bcc0fee55bf0b51009b0abeaabcf78800b01533f
Mikhail Vasiliev <mevasiliev@mail.ru>
2023-01-18 10:13:54 +0800

    Added Task04

---
+++ Task04/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Enter number 1: ");
+int number1 = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("Enter number 2: ");
+int number2 = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("Enter number 3: ");
+int number3 = Convert.ToInt32 (Console.ReadLine());
+int tmp = number1;
+
+if (number2 > tmp)
+{
+    tmp = number2;
+}
+if (number3 > tmp)
+{
+    tmp = number3;
+}   
+Console.WriteLine($"Max = {tmp}");
