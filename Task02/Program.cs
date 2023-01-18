4264dbf4704a6258f681ff1adebbaa1e4ef8c20b
Mikhail Vasiliev <mevasiliev@mail.ru>
2023-01-18 10:13:09 +0800

    Added Task02

---
+++ Task02/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Enter number 1: ");
+int number1 = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("Enter number 2: ");
+int number2 = Convert.ToInt32 (Console.ReadLine());
+
+if (number1 > number2)
+{
+    Console.WriteLine($"Max = {number1}");
+}
+else 
+{
+    Console.WriteLine($"Max = {number2}");
+}
