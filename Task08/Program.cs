1070d32b12c041970e064149d474f1a56635d883
Mikhail Vasiliev <mevasiliev@mail.ru>
2023-01-18 10:14:45 +0800

    Added Task08

---
+++ Task08/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Enter N:");
+int number = Convert.ToInt32(Console.ReadLine());
+int i = 2;
+if (number > 0)
+{
+    while (i <= number)
+    {
+        Console.Write($"{i} ");
+        i += 2;
+    }
+}
+else
+{
+    Console.Write("You entered a number < 0 ");
+}
