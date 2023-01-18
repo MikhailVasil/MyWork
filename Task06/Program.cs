22ab18a7c3c18b2fe90fdae68a71c49f650120e8
Mikhail Vasiliev <mevasiliev@mail.ru>
2023-01-18 10:14:20 +0800

    Added Task06

---
+++ Task06/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Enter number : ");
+int number1 = Convert.ToInt32 (Console.ReadLine());
+
+if (number1 == 0)
+{
+    Console.WriteLine($"You entered {number1}");
+}
+else if (number1 % 2 == 0)
+{
+    Console.WriteLine($"{number1} is even");
+}
+else 
+{
+    Console.WriteLine($"{number1} is not even");
+}
