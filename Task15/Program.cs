1a598e31ae68f9137c2ef7e953e36f39e815c248
Mikhail Vasiliev <mevasiliev@mail.ru>
2023-01-23 22:46:09 +0800

    Added Task15

---
+++ Task15/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 15: Напишите программу, 
+// которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, 
+// является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Enter a number from 1 to 7");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number == 6 || number == 7)
+{
+    Console.WriteLine("yes");
+}
+else if (number>=1 && number<6)
+{
+    Console.WriteLine("No");
+}
+else 
+{
+    Console.WriteLine("Fail");
+}
+// bool weekend = Weekend(number);
+
+// if (weekend >=1 && weekend <=7){
+// Console.WriteLine("Yes");
+// }
+// else {
+//     Console.WriteLine("No");
+// }
+// bool Weekend(number)
+// {
+//     return number == 6 && number == 7;
+// }
