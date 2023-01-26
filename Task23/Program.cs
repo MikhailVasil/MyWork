52a9bb59eaefc7c5c3df07ff7c4f9a8704188d51
Mikhail Vasiliev <mevasiliev@mail.ru>
2023-01-26 12:11:12 +0800

    Added Task23

---
+++ Task23/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Enter the digit:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 1) Console.WriteLine("Invalid input");
+else Cube(number);
+
+void Cube(int number)
+{
+    int n = 1;
+    while (n <= number)
+    {
+        Console.WriteLine($"{n,3} {n * n * n,6}");
+        n++;
+    }
+}
