9bdf5362cb7e33d7f07eca59f28acd68c8e1d62b
Mikhail Vasiliev <mevasiliev@mail.ru>
2023-01-26 12:10:05 +0800

    Added Task19

---
+++ Task19/Program.cs
@@ -0,0 +1,41 @@
+﻿// Напишите программу, которая принимает на 
+// вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.WriteLine("Enter palindrome: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 0 || number > 2000000000)
+{
+    Console.WriteLine("Input Error");
+}
+else
+{
+    Equality(number, MirrorDigit(number));
+}
+
+int MirrorDigit(int num)
+{
+    int mirror = 0;
+    while (num != 0)
+    {
+        int last = num % 10;
+        mirror = mirror * 10 + last;
+        num /= 10;
+    }
+    return mirror;
+
+}
+
+void Equality(int arg1, int arg2)
+{
+    if (arg1 == arg2)
+    {
+        Console.WriteLine("Yes");
+    }
+    else
+    {
+        Console.WriteLine("No");
+    }
+}
