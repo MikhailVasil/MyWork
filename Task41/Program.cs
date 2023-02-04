fea19646b3a9c0f6d21fdd989a1aa6d56797d4dd
Mikhail Vasiliev <mevasiliev@mail.ru>
2023-02-04 11:20:47 +0800

    Added Task41

---
+++ Task41/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 41: Пользователь вводит с 
+// клавиатуры M чисел. Посчитайте, 
+// сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// -1, -7, 567, 89, 223-> 3
+
+Console.Write("Enter quantity of numbers:");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[] inputArr = InputArray(size);
+PrintArray(inputArr);
+int naturalNumbers = NaturalNumbers(inputArr);
+Console.WriteLine($" -> {naturalNumbers}");
+
+int[] InputArray(int number)
+{
+    int[] array = new int[number];
+    for (int i = 0; i < number; i++)
+    {
+        array[i] = Convert.ToInt32(Console.ReadLine());
+
+    }
+    return array;
+}
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+}
+int NaturalNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) count++;
+    }
+    return count;
+}
+
